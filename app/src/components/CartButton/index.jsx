17a5b92e0ca8a485b5eb19d5be85dc091a6a5277
Marlon Willian Silva Barros <mwsb2004@gmail.com>
2024-05-14 20:11:38 -0300

    preços arrumados e botão de adicionar ao carrinho na página game

--- app/src/components/CartButton/index.jsx
+++ app/src/components/CartButton/index.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../context/Cart";
 import styles from "./CartButton.module.css";
+import { useState } from "react";
 
 export const convertPrice = (price) => Number(price).toFixed(2).replace('.', ',');
 
-function CartButton({margintop, marginleft, discount, price, id}) {
+function CartButton({id, price, discount, gamePage, margintop, marginleft}) {
     let desconto = discount/100
     desconto = desconto * price  
     let newPrice = price - desconto
@@ -12,34 +13,69 @@ function CartButton({margintop, marginleft, discount, price, id}) {
     const { inCart, addCart } = useCartContext()
     const noCart = inCart.some((cart) => cart.id === id)
     const icone = noCart ? "fa-solid fa-xmark" : "fa-solid fa-cart-plus"
-    
+
+    const [ buy, setBuy ] = useState(noCart ? true : false);
+
     return (
-        <button 
-            className={styles.btn} 
-            style={{ marginTop: margintop, marginLeft: marginleft}}
-            onClick={typeof(price) == "number" ? () => addCart({id}) : null}
-        >
-            {
-                typeof(discount) == "number" ? 
-                <span className={styles.discount}> -{discount}% </span> : null
-            }
-            {
-                typeof(price) == "number" ?
-                <>
-                    <span className={styles.price}>
-                        R$ {
-                            typeof(discount) == "number" ? 
-                            `${convertPrice(newPrice)}` : `${convertPrice(price)}`
-                        }
-                    </span>
-                    <i class={`${icone}`} style={{color: noCart ? "red" : "white"}}></i>
-                </> 
-                : 
-                <Link to={`/jogo/${id}`}>
-                    <span style={{padding: "5px"}}>Anunciado</span>
-                </Link>
-            }
-        </button>
+        <>
+            <button 
+                className={styles.btn}
+                style={{
+                    display: buy == true ? "block" : "none", 
+                    width: "auto", textAlign: "center", 
+                    fontSize: "20px", marginRight: "30px"
+                }}
+            >
+                <span style={{paddingRight: "10px"}}> 
+                    <i 
+                        class="fa-solid fa-check" 
+                        style={{marginInline: "10px", color: "green"}}
+                    >
+                    </i>
+                    Comprar
+                </span>
+            </button>
+            <button
+                className={styles.btn}
+                style={{ marginTop: margintop, marginLeft: marginleft}}
+                onClick={
+                    typeof(price) == "number" && buy == false ? 
+                    () => { addCart({id}); setBuy(true);} 
+                    : typeof(price) == "number" && buy == true ? 
+                    () => { addCart({id}); setBuy(false);} : null
+                }
+                type="button"
+            >
+            
+                {
+                    typeof(discount) == "number" && gamePage != true ?
+                    <span className={styles.discount}> -{discount}% </span> : null
+                }
+                {
+                    typeof(price) == "number" && gamePage != true ?
+                    <>
+                        <span className={styles.price}>
+                            R$ {
+                                typeof(discount) == "number" ?
+                                `${convertPrice(newPrice)}` : `${convertPrice(price)}`
+                            }
+                        </span>
+                        <i class={`${icone}`} style={{color: noCart ? "red" : "white"}}></i>
+                    </>
+                    : typeof(price) == "number" || "object" && gamePage == true ?
+                    <>
+                        <span className={styles.price}>
+                            {noCart? "Remover" : "Adicionar ao Carrinho"}
+                        </span>
+                        <i class={`${icone}`} style={{color: noCart ? "red" : "white"}}></i>
+                    </>
+                    : typeof(price) === "string" ?
+                    <Link to={`/jogo/${id}`}>
+                        <span style={{padding: "5px"}}>Anunciado</span>
+                    </Link> : null
+                }
+            </button>
+        </>
     );
 }
 
--- app/src/components/Form/index.jsx
+++ app/src/components/Form/index.jsx
@@ -2,11 +2,11 @@ import styles from "./Form.module.css";
 import jogo from "../../json/games.json"
 import FilterPlataform from "../FilterPlataform";
 import React, { useState } from 'react';
+import CartButton, { convertPrice } from "../CartButton";
 
 function Form({ id }) {
     const [valorSelecionado, setValorSelecionado] = useState('');
     const opcoes = jogo[id].preco;
-    console.log(typeof(opcoes))
     
     const handleChange = (event) => {
         setValorSelecionado(event.target.value);
@@ -18,29 +18,37 @@ function Form({ id }) {
             <h5>{jogo[parseInt(id)].developer}</h5>
             <form className={styles.edition}>
                 <FilterPlataform id={id} fontsize="13px"/>
+                <hr/>
                 <div>
-                    <hr/>
                     {
                         typeof(opcoes) == "object" ?
-                            opcoes.map(opcao => (
+                            opcoes.map((opcao, index) => (
                                 <label key={opcao} checked>
                                     <input
                                         type="radio"
                                         value={opcao}
-                                        checked={valorSelecionado === opcao}
+                                        checked={valorSelecionado == opcao}
                                         onChange={handleChange}
                                     />
-                                    {opcao}
+                                    <span>
+                                        R$ {convertPrice(opcao)} | {index === 0 ? "Edição Padrão" : "Edição Deluxe"}
+                                    </span>
                                 </label> 
-                            )) : <span className={styles.price}>{jogo[id].preco}</span>
+                            )) : <span className={styles.price}>R$ {convertPrice(jogo[id].preco)}</span>
                     }
-                    <hr/>
-                    <button type="submit" className={styles.submitButton}>
-                        Adicionar ao carrinho
-                    </button>
+                    <section style={{display: "inline-flex", alignItems: "flex-end"}}>
+                        <CartButton
+                            id={id}
+                            price={
+                                typeof(jogo[id].preco) == "number" ? jogo[id].preco
+                                : typeof(jogo[id].preco) == "object" ? jogo[id].preco[0] : null
+                            }
+                            gamePage={true}
+                            margintop="30px"
+                        />
+                    </section>
                 </div>
             </form>
-            {console.log(parseInt(id))}
         </div>
     );
 }
